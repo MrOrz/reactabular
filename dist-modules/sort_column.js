278811f33910203522cb3029739a8c191c2dfbee
MrOrz <johnsonliang7@gmail.com>
2016-02-14 13:46:49 +0800

    deploy

---
+++ dist-modules/sort_column.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function (columns, column, done) {
+    // reset old classes
+    columns.forEach(function (col) {
+        col.headerClass = null;
+    });
+
+    column.sort = column.sort === 'asc' ? 'desc' : 'asc';
+
+    // push sorting hint
+    column.headerClass = 'sort-' + column.sort;
+
+    done({
+        sortingColumn: column,
+        columns: columns
+    });
+};
+
+// sorter === lodash sortByOrder
+// https://lodash.com/docs#sortByOrder
+module.exports.sort = function (data, column, sorter) {
+    if (!column) {
+        return data;
+    }
+
+    return sorter(data, [column.property], [column.sort]);
+};
---
+++ dist-modules/sort_columns.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
+module.exports = function (columns, sortColumns, column, done) {
+    var newSortCols = undefined;
+
+    if (typeof sortColumns === 'undefined') {
+        newSortCols = [column];
+    } else if (sortColumns.includes(column)) {
+        newSortCols = sortColumns;
+    } else {
+        newSortCols = [].concat(_toConsumableArray(sortColumns), [column]);
+    }
+
+    // cycle through: asc, desc, no sort
+    if (typeof column.sort === 'undefined' || column.sort === '') {
+        column.sort = 'asc';
+        column.headerClass = 'sort-asc';
+    } else if (column.sort === 'asc') {
+        column.sort = 'desc';
+        column.headerClass = 'sort-desc';
+    } else {
+        var idx = newSortCols.indexOf(column);
+        if (idx > -1) {
+            newSortCols.splice(idx, 1);
+        }
+        column.headerClass = null;
+        column.sort = '';
+    }
+
+    done({
+        sortingColumns: newSortCols,
+        columns: columns
+    });
+};
+
+// sorter === lodash sortByOrder
+// https://lodash.com/docs#sortByOrder
+module.exports.sort = function (data, sortColumns, sorter) {
+    if (!sortColumns) {
+        return data;
+    }
+
+    var propertyList = [];
+    var orderList = [];
+
+    sortColumns.forEach(function (column) {
+        propertyList.push(column.property);
+        orderList.push(column.sort);
+    });
+
+    return sorter(data, propertyList, orderList);
+};
